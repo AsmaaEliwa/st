f2db027b51f66432751c62c53e267ef5964f3ba1
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-06 12:42:11 -0400

    sum_rec rb

--- sum_rec.rb
+++ sum_rec.rb
@@ -4,4 +4,12 @@ def sum_rec(arr)
     return arr[0] if arr.length==1
     return arr[0]+sum_rec(arr[1..-1])
 end
+
+def sum_rec(arr)
+  sum=0
+  arr.each do |ele|
+    sum+=ele
+  end
+  sum
+end
 puts  sum_rec([1,2,3,4])
