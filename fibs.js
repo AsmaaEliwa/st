1207ece47cec45a1d1ea5918df6f6fdc52ebe5b4
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-06 18:26:04 -0400

    fibs js

---
+++ fibs.js
@@ -0,0 +1,12 @@
+// Write a function, fibs(num) which returns the first n elements from the fibonnacci sequence, given n.
+// Solve it both iteratively and recursively.
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+function  fibs(num){
+    const holder=[0,1];
+    for (let i=0 ; i<num-2 ;i++){
+        holder.push(holder[holder.length-1]+holder[holder.length-2])
+    }
+    return holder
+}
+console.log(fibs(9))
---
+++ fibs.rb
@@ -0,0 +1,2 @@
+# Write a function, fibs(num) which returns the first n elements from the fibonnacci sequence, given n.
+# Solve it both iteratively and recursively.
