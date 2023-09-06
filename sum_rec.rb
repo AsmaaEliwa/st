cef4258f3f079aa3caf7691a6d46c0ce1484c8fc
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-06 12:37:44 -0400

    sum_rec rb

--- sum_rec.rb
+++ sum_rec.rb
@@ -1,1 +1,7 @@
 # Write a function that takes an array of integers and returns their sum. Use recursion.
+
+def sum_rec(arr)
+    return arr[0] if arr.length==1
+    return arr[0]+sum_rec(arr[1..-1])
+end
+puts  sum_rec([1,2,3,4])
