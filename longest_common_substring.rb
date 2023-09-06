cefe2582f67a6f4fadc24ea5be54c90a990a64df
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-06 12:47:32 -0400

    added another solution

--- longest_common_substring.rb
+++ longest_common_substring.rb
@@ -56,3 +56,34 @@ def longest_common_substring(str1, str2)
   
     longest_substring
   end
+
+#   dynamic imp
+def make_matrix(str1, str2)
+    matrix = Array.new(str1.length + 1) { Array.new(str2.length + 1, 0) }
+  
+    str1.chars.each_with_index do |el1, idx1|
+      str2.chars.each_with_index do |el2, idx2|
+        if el1 == el2
+          matrix[idx1 + 1][idx2 + 1] = matrix[idx1][idx2] + 1
+        else
+          matrix[idx1 + 1][idx2 + 1] = 0
+        end
+      end
+    end
+  
+    matrix
+  end
+  
+  def longest_common_substring(str1, str2)
+    matrix = make_matrix(str1, str2)
+    greatest_substring = ""
+    matrix.each_with_index do |row, idx1|
+      row.each_with_index do |length, idx2|
+        if length > greatest_substring.length
+          greatest_substring = str2[idx2 - length...idx2]
+        end
+      end
+    end
+  
+    greatest_substring
+  end
