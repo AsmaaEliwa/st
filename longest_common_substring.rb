d2cb49d0821edd9f4e077555ac4b4b8f4360e39d
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-06 12:46:48 -0400

    added another solution

--- longest_common_substring.rb
+++ longest_common_substring.rb
@@ -30,3 +30,29 @@ puts
 puts substrings("hello")
 puts
 puts longest_common_substring("str1","str2")
+
+
+# another solution 
+
+def longest_common_substring(str1, str2)
+    longest_substring = ""
+  
+    start_idx = 0
+    while start_idx < str1.length
+      # don't consider substrings that would be too short to beat
+      # current max.
+      len = longest_substring.length + 1
+  
+      while (start_idx + len) <= str1.length
+        end_idx = start_idx + len
+        substring = str1[start_idx...end_idx]
+        longest_substring = substring if str2.include?(substring)
+  
+        len += 1
+      end
+  
+      start_idx += 1
+    end
+  
+    longest_substring
+  end
