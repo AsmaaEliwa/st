c96b68ce720a133ba5521b7dd492ef1910e9c0d9
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-07 11:07:15 -0400

    sum_from_file js

--- sum_from_file.js
+++ sum_from_file.js
@@ -1,1 +1,19 @@
 // Write a method that reads in a file of integers, one per line, and sums them. Skip the line if it begins with a "#"
+const fs = require('fs');
+function sum_from_file(filename){
+    let sum=0
+    const data=fs
+    .readFileSync(filename, 'utf-8').split('\n')
+    data.map(ele=> {
+        if(ele[0] !== "#"){
+            const number=Number(ele)
+            sum+=number
+        }
+       
+
+    })
+    return sum
+}
+
+file = "./file.txt"
+console.log( sum_from_file(file))
