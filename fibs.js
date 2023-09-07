1c0bea14d372969a3721134cb14052daf04b6d45
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-07 09:44:13 -0400

    fibs js recursively

--- fibs.js
+++ fibs.js
@@ -1,7 +1,7 @@
 // Write a function, fibs(num) which returns the first n elements from the fibonnacci sequence, given n.
 // Solve it both iteratively and recursively.
 
-// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 function  fibs(num){
     const holder=[0,1];
     for (let i=0 ; i<num-2 ;i++){
@@ -9,10 +9,14 @@ function  fibs(num){
     }
     return holder
 }
-console.log(fibs(9))
 
 function fibs(num){
     if(num===2)return [0,1]
-    if (num==1)return [0]
-    return fibs(num)
+    if (num===1)return [0]
+    if (num===2) return [0,1]
+    const prev= fibs(num-1)
+    const next = prev[prev.length-1] + prev[prev.length-2]
+    prev.push(next)
+    return prev
 }
+console.log(fibs(10))
