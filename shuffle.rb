85cb8f2c828b87a12452c4297ff8a276837a27bb
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-07 11:23:40 -0400

    shuffle rb

--- shuffle.rb
+++ shuffle.rb
@@ -1,1 +1,16 @@
 # You are given an array and a random number generator. Shuffle the array.
+def shuffle(arr)
+    array=arr.dup
+    arrLength=arr.length 
+    i=arrLength-1
+    while i>=0
+        randomIndex=rand(0...arrLength)
+        array[i],array[randomIndex]=array[randomIndex],array[i]
+        i-=1
+    end
+    array
+end
+
+ myArray = [1, 2, 3, 4, 5];
+ shuffledArray = shuffle(myArray);
+puts (shuffledArray);
