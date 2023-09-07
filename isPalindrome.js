48f5a5a012060e506d8743ee4b981a98d7b42c86
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-07 10:01:41 -0400

    isPalindrome js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,14 @@
 // Write a JavaScript function that takes a string and returns true if it's a palindrome, false if it's not. Use JavaScript.
+function isPalindrome(str){
+    let newStr=""
+    const arr=str.split("")
+    let i=arr.length-1 
+    while (i>=0 ){
+        newStr+= arr[i]
+        i-=1
+    }
+    return str===newStr
+}
+
+console.log(isPalindrome("str"))
+console.log(isPalindrome("level"))
