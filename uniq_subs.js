217496fb226a75f3678f6c8f037e5c8b0ef7cb19
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-08 12:46:41 -0400

    uniq_subs js

---
+++ uniq_subs.js
@@ -0,0 +1,16 @@
+// # Write a method that finds all the unique substrings for a word.
+function uniqe_subs(word){
+    const holder=[]
+    for (let i=0 ; i<word.length ; i++){
+        for (let j=i ; j<word.length ; j++){
+            let str=""
+           for(let k=i ;k<=j ; k++){
+               str+=word[k]
+           }
+           holder.push(str)
+        }
+    }
+    return holder
+}
+
+console.log(uniqe_subs("word"))
