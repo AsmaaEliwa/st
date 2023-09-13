5a9d53390aa90662d5258368820f7cb6953d7420
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-09-13 10:32:10 -0400

    Memoizing Factorial

--- factorial.js
+++ factorial.js
@@ -3,10 +3,10 @@
 // For example, `factorial(5)` is 5 * 4 * 3 * 2 * 1 = 120.
 
 
-// function factorial(n){
-//     if (n===1)return 1
-//     return n*factorial(n-1)
-// }
+function factorial(n){
+    if (n===1)return 1
+    return n*factorial(n-1)
+}
 
 //Memoizing Factorial (kind of)
 let memo={}
